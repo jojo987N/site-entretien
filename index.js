fa038ed904f6244c74eebc4fc48175a01b868851
app-tech5 <eboagalle@gmail.com>
2024-05-16 12:42:00 +0200

    add screen in javascript

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('hi')
+
+window.onload = () => {
+    if (window.screen.width < 768)
+    document.querySelector('body').innerHTML = `
+    <div>
+        <h1>Je ne suis pas encore disponible pour le mobile, utilise un ordinateur</h1>
+    </div>
+    `
+}
